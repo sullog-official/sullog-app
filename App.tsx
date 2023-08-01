37d9fb51a8e6ef03a7739e956ac443aa92d4a33c
Jeon <gino9940@gmail.com>
2023-08-01 15:21:36 +0900

    feat: add cookie setting

--- App.tsx
+++ App.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
-import WebView from 'react-native-webview';
+import React, {createRef} from 'react';
+import {NativeSyntheticEvent} from 'react-native';
+import WebView, {WebViewNavigation} from 'react-native-webview';
+import {WebViewMessage} from 'react-native-webview/lib/WebViewTypes';
 
 const App = () => {
-  const uri = 'https://sullog-client.vercel.app/';
+  const uri = 'http://localhost:3000/login';
 
-  return <WebView source={{uri}} />;
+  // Send the cookie information back to the mobile app
+  const CHECK_COOKIE: string = `
+    ReactNativeWebView.postMessage("Cookie: " + document.cookie);
+    true;
+  `;
+
+  const onNavigationStateChange = (navigationState: WebViewNavigation) => {
+    console.log(
+      '🚀 ~ file: App.tsx:16 ~ onNavigationStateChange ~ navigationState:',
+      navigationState,
+    );
+    // Check cookies every time URL changes
+    if (webViewRef.current) {
+      webViewRef.current.injectJavaScript(CHECK_COOKIE);
+    }
+  };
+
+  const onMessage = (event: NativeSyntheticEvent<WebViewMessage>) => {
+    const {data} = event.nativeEvent;
+
+    if (data.includes('Cookie:')) {
+      // process the cookies
+    }
+  };
+
+  let webViewRef = createRef<WebView>();
+
+  return (
+    <WebView
+      ref={webViewRef}
+      source={{uri}}
+      onNavigationStateChange={onNavigationStateChange}
+      onMessage={onMessage}
+      thirdPartyCookiesEnabled
+      sharedCookiesEnabled
+    />
+  );
 };
 
 export default App;
