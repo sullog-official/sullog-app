14424b379fe32f99dd4c66e26646e68d4ca5e4f3
Jeon <gino9940@gmail.com>
2023-08-04 16:03:39 +0900

    fix: webview uri

--- App.tsx
+++ App.tsx
@@ -1,43 +1,44 @@
 import React, {createRef} from 'react';
-import {NativeSyntheticEvent} from 'react-native';
-import WebView, {WebViewNavigation} from 'react-native-webview';
-import {WebViewMessage} from 'react-native-webview/lib/WebViewTypes';
+import WebView from 'react-native-webview';
 
 const App = () => {
-  const uri = 'http://localhost:3000/login';
-
-  // Send the cookie information back to the mobile app
-  const CHECK_COOKIE: string = `
-    ReactNativeWebView.postMessage("Cookie: " + document.cookie);
-    true;
-  `;
-
-  const onNavigationStateChange = (navigationState: WebViewNavigation) => {
-    console.log(
-      '🚀 ~ file: App.tsx:16 ~ onNavigationStateChange ~ navigationState:',
-      navigationState,
-    );
-    // Check cookies every time URL changes
-    if (webViewRef.current) {
-      webViewRef.current.injectJavaScript(CHECK_COOKIE);
-    }
-  };
-
-  const onMessage = (event: NativeSyntheticEvent<WebViewMessage>) => {
-    const {data} = event.nativeEvent;
-
-    if (data.includes('Cookie:')) {
-      // process the cookies
-    }
-  };
+  const uri = 'https://sullog-client.vercel.app/';
 
   let webViewRef = createRef<WebView>();
+
+  const debugging = `
+  const consoleLog = (type, log) => window.ReactNativeWebView.postMessage(JSON.stringify({'type': 'Console', 'data': {'type': type, 'log': log}}));
+  console = {
+      log: (log) => consoleLog('log', log),
+      debug: (log) => consoleLog('debug', log),
+      info: (log) => consoleLog('info', log),
+      warn: (log) => consoleLog('warn', log),
+      error: (log) => consoleLog('error', log),
+    };
+`;
+
+  const onMessage = (payload: {nativeEvent: {data: string}}) => {
+    let dataPayload;
+    try {
+      dataPayload = JSON.parse(payload.nativeEvent.data);
+    } catch (e) {}
+
+    if (dataPayload) {
+      if (dataPayload.type === 'Console') {
+        console.info(`[Console] ${JSON.stringify(dataPayload.data)}`);
+      } else {
+        console.log(dataPayload);
+      }
+    }
+  };
 
   return (
     <WebView
       ref={webViewRef}
-      source={{uri}}
-      onNavigationStateChange={onNavigationStateChange}
+      source={{
+        uri,
+      }}
+      injectedJavaScript={debugging}
       onMessage={onMessage}
       thirdPartyCookiesEnabled
       sharedCookiesEnabled
